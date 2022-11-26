093a5de4e0aac2cfba533f25566e87dfeb460d31
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2022-11-26 15:47:30 -0500

    Add files via upload

---
+++ Transaction.h
@@ -0,0 +1,14 @@
+//
+// Created by Tyler MacPhee on 11/17/22.
+//
+
+class Transaction {
+public:
+    double withdrawal(double balance, double amount);
+    double deposit(double balance, double amount);
+};
+
+#ifndef ATMAPP_TRANSACTION_H
+#define ATMAPP_TRANSACTION_H
+
+#endif //ATMAPP_TRANSACTION_H
